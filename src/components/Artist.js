ab2c2f74492be978eabf3085e576102a4001c9f6
Neil Williams <neil.williams@dvla.gsi.gov.uk>
2018-11-15 15:14:04 +0000

    Added new artist and delete artist

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -40,6 +40,37 @@ export default class Artist extends React.Component {
       }
       this.fetchSongsByArtist()
     };
+
+    this.deleteHandled = (e, artist_id) => {
+      console.log("going in delete");
+      e.preventDefault();
+
+      this.setState({
+        showContent: 'none'
+      });
+
+      this.deleteArtist = function(artist_id){
+        fetch('/artist/' + artist_id, {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(res => res.json)
+        .then(artist => {
+          console.log(artist);
+          this.props.fetchArtists();
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({
+            error: err
+          })
+        });
+      }
+
+      this.deleteArtist(artist_id);
+    }
   }
 
   presentAlbums(data) {
@@ -60,6 +91,7 @@ export default class Artist extends React.Component {
       <li>
         <div className="collapsible-header" onClick={(e) => this.showCollapsibleContent(e,this.props.artist_id)} tabIndex="0">
           <i className="material-icons">filter_drama</i>{this.props.name}
+          <button className="waves-effect waves-light btn-small right red darken-2" type="button" onClick={(e) => this.deleteHandled(e,this.props.artist_id)}>Delete</button>
         </div>
         <div className="collapsible-body" style={{display: this.state.showContent}}>
           {this.presentAlbums(this.state.albums)}
